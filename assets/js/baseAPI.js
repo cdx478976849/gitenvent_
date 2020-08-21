afd1258d4329166aeaae0bb04e2816ab5eb40ceb
chdexiang <478976849@qq.com>
2020-08-21 12:59:52 +0800

    完成了主页的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,21 @@
 var baseURL = 'http://ajax.frontend.itheima.net';
 $.ajaxPrefilter(function (o) {
     o.url = baseURL + o.url;
+
+    if (o.url.indexOf('/my/') !== -1) {
+        o.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        }
+    }
+
+
+    o.complete = function (res) {
+        console.log(res.responseJSON);
+        console.log(res);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == "身份认证失败！") {
+            localStorage.removeItem('token');
+            location = '/login.html';
+        }
+    }
 })
